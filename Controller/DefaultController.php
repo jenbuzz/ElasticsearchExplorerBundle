1191c19db75b9b9a1c3e56f4b3d406fb776990df
dan-lyn <daniel_lynge@hotmail.com>
2014-12-02 19:40:59 +0100

    instantiating the elasticsearch client (test)

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -8,11 +8,15 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $client = new \Elasticsearch\Client();
+
         return $this->render('DanLynElasticsearchExplorerBundle:Default:index.html.twig');
     }
 
     public function searchAction($searchterm = false)
     {
+        $client = new \Elasticsearch\Client();
+
         return $this->render('DanLynElasticsearchExplorerBundle:Default:search.html.twig', array('searchterm' => $searchterm));
     }
 }
