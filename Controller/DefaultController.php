789c4ff961a8ecf02907d7e9cf4f1666c25a4e2b
dan-lyn <daniel_lynge@hotmail.com>
2014-12-06 19:49:55 +0100

    show all indexes (+ sizes) on startpage

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -9,8 +9,22 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $client = new \Elasticsearch\Client();
+        $objIndexes = $client->indices();
+        $arrStats = $objIndexes->stats();
+        $arrIndexesStats = $arrStats['indices'];
+        
+        $arrIndexes = array();
+        foreach ($arrIndexesStats AS $indexKey => $indexValues) {
+          $arrIndexes[] = array(
+            'name' => $indexKey,
+            'total_docs' => $indexValues['total']['docs']['count'],
+            'total_size' => $indexValues['total']['store']['size_in_bytes'],
+          );
+        }
 
-        return $this->render('DanLynElasticsearchExplorerBundle:Default:index.html.twig');
+        return $this->render('DanLynElasticsearchExplorerBundle:Default:index.html.twig', array(
+          'indexes' => $arrIndexes,
+        ));
     }
 
     public function searchAction($searchterm = false)
