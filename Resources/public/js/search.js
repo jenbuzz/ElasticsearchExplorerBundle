5f84a653983dde4c8be41971af188a0a383f46dc
dan-lyn <daniel_lynge@hotmail.com>
2014-12-30 20:11:51 +0100

    check for same url segment

--- Resources/public/js/search.js
+++ Resources/public/js/search.js
@@ -1,11 +1,13 @@
 $('#search-select-index').on('change', function() {
-  if ($(this).val()!=-1) {
+  var arrUrl = $(location).attr('href').split('/');
+  if ($(this).val()!=-1 && $(this).val()!=arrUrl[arrUrl.length-2]) {
     document.location+= $(this).val();
   }
 });
 
 $('#search-select-type').on('change', function() {
-  if ($(this).val()!=-1) {
+  var arrUrl = $(location).attr('href').split('/');
+  if ($(this).val()!=-1 && $(this).val()!=arrUrl[arrUrl.length-2]) {
     document.location+= $(this).val();
   }
 });
