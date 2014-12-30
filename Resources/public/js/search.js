a62fa2abb93b76a7f9cdc6bf21f8b888de0d3a37
dan-lyn <daniel_lynge@hotmail.com>
2014-12-30 19:56:53 +0100

    js redirect on selectbox change

--- Resources/public/js/search.js
+++ Resources/public/js/search.js
@@ -1,1 +1,7 @@
-console.log("search");
+$('#search-select-index').on('change', function() {
+  document.location+= $(this).val();
+});
+
+$('#search-select-type').on('change', function() {
+  document.location+= $(this).val();
+});
