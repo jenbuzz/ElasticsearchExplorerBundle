234dce565c6d56194b4e9a1efd63eda9bc0fefb6
dan-lyn <daniel_lynge@hotmail.com>
2015-01-09 19:53:52 +0100

    setup grunt plugin uglify to minify search.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,16 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: 'Resources/public/js/search.js',
+        dest: 'Resources/public/js/search.min.js'
+      }
+    }
   });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('minify', ['uglify']);
 
 };
